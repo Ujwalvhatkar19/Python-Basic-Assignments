6a8a6ecc3b54821d777039e795b85718967bd84e
Ujwalvhatkar19 <95517758+Ujwalvhatkar19@users.noreply.github.com>
2022-09-21 18:18:33 +0530

    Add files via upload

---
+++ PB_Assignment 3.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Que 1:
+    get_ipython().set_next_input('    Why are functions advantageous to have in your programs');get_ipython().run_line_magic('pinfo', 'programs')
+
+
+# In[ ]:
+
+
+Answer1:
+    It makes programme shorter, easy to read, easy to update and it avoid duplicate work also.
+
+
+# In[ ]:
+
+
+Que 2:
+    get_ipython().set_next_input('    When does the code in a function run: when it is specified or when it is called');get_ipython().run_line_magic('pinfo', 'called')
+
+
+# In[ ]:
+
+
+Answer 2:
+    when it is callled.
+
+
+# In[ ]:
+
+
+Que 3:
+    get_ipython().set_next_input('    What statement creates a function');get_ipython().run_line_magic('pinfo', 'function')
+
+
+# In[ ]:
+
+
+Answer 3:
+    A def keyword creates a function 
+
+
+# In[ ]:
+
+
+Que 4:
+    get_ipython().set_next_input('    What is the difference between a function and a function call');get_ipython().run_line_magic('pinfo', 'call')
+
+
+# In[ ]:
+
+
+Answer 4:
+    A function is a block of code that does a particular operation and returns a result.
+    A function call is the code used to pass control to a function.
+
+
+# In[ ]:
+
+
+Que 5:
+    get_ipython().set_next_input('    How many global scopes are there in a Python program? How many local scopes');get_ipython().run_line_magic('pinfo', 'scopes')
+
+
+# In[ ]:
+
+
+Answer 5:
+    There is one global scope, and a local scope is created whenever a function is called.
+
+
+# In[ ]:
+
+
+Que 6:
+    get_ipython().set_next_input('    What happens to variables in a local scope when the function call returns');get_ipython().run_line_magic('pinfo', 'returns')
+
+
+# In[ ]:
+
+
+Answer 6:
+    When a function returns, the local scope is destroyed, 
+    and all the variables in it are forgotten.
+
+
+# In[ ]:
+
+
+Que 7:
+    What is the concept of a return value? Is it possible to have a return 
+    get_ipython().set_next_input('    value in an expression');get_ipython().run_line_magic('pinfo', 'expression')
+
+
+# In[ ]:
+
+
+Answer7:
+    A return value is the value that a function call evaluates to.
+    Like a return value can be used as part of an expression.
+
+
+# In[ ]:
+
+
+Que 8:
+    If a function does not have a return statement, 
+    get_ipython().set_next_input('    what is the return value of a call to that function');get_ipython().run_line_magic('pinfo', 'function')
+
+
+# In[ ]:
+
+
+Answer 8:
+    If there is no return statement for a function, its return value is None.
+
+
+# In[ ]:
+
+
+Que 9:
+    get_ipython().set_next_input('    How do you make a function variable refer to the global variable');get_ipython().run_line_magic('pinfo', 'variable')
+
+
+# In[ ]:
+
+
+Answer 9:
+    A global statement will force a variable in a function to refer to the global variable.
+
+
+# In[ ]:
+
+
+Que 10:
+    get_ipython().set_next_input('    What is the data type of None');get_ipython().run_line_magic('pinfo', 'None')
+
+
+# In[ ]:
+
+
+Answr 10:
+    None type
+
+
+# In[ ]:
+
+
+Que 11:
+    get_ipython().set_next_input('    What does the sentence import areallyourpetsnamederic do');get_ipython().run_line_magic('pinfo', 'do')
+
+
+# In[ ]:
+
+
+Answer 11:
+    import statement will import areallyourpetsnamederic
+
+
+# In[ ]:
+
+
+Que 12:
+    get_ipython().set_next_input('    If you had a bacon() feature in a spam module, what would you call it after importing spam');get_ipython().run_line_magic('pinfo', 'spam')
+
+
+# In[ ]:
+
+
+Answer 12:
+    The function will be called spam.bacon()
+
+
+# In[ ]:
+
+
+Que 13:
+    get_ipython().set_next_input('    What can you do to save a programme from crashing if it encounters an error');get_ipython().run_line_magic('pinfo', 'error')
+
+
+# In[ ]:
+
+
+Answer 13:
+    Place the line of code that might cause an error in a try clause.
+
+
+# In[ ]:
+
+
+Que 14:
+    get_ipython().set_next_input('    What is the purpose of the try clause? What is the purpose of the except clause');get_ipython().run_line_magic('pinfo', 'clause')
+
+
+# In[ ]:
+
+
+Answer 14:
+    The code that could potentially cause an error goes in the try clause.
+    The code that executes if an error happens goes in the except clause.
+
