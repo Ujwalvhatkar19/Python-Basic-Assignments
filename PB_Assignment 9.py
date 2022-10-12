d787e63d411401eb2343b4d6a7bbce821d943257
Ujwalvhatkar19 <95517758+Ujwalvhatkar19@users.noreply.github.com>
2022-10-12 17:29:49 +0530

    Add files via upload

---
+++ PB_Assignment 9.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Que 1: To what does a relative path refer?
+
+# In[ ]:
+
+
+Answer 1:
+relative path refers to current working file
+
+
+# # Que 2 : What does an absolute path start with your operating system?
+
+# In[ ]:
+
+
+it starts root file with C:/
+
+
+# # Que 3: What do the functions os.getcwd() and os.chdir() do?
+
+# In[ ]:
+
+
+Answer 3:
+os.chdir() changes the current working directory
+os.getcwd after changing the direction it tells the changed current working directoty path. 
+
+
+# # Que 4: What are the . and .. folders?
+
+# In[ ]:
+
+
+Answer 4:
+The . folder is the current folder, and .. is the parent folder
+
+
+# # Que 5:In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+
+# In[ ]:
+
+
+Answer 5:
+C:\bacon\eggs is a dir name & spam.txt is the base name
+
+
+# # Que 6: What are the three “mode” arguments that can be passed to the open() function?
+
+# In[ ]:
+
+
+The string 'r' is for read only
+'w' is for write only
+'r+' is for reading and writing
+'a' is for apend
+'w+' is for writing and reading
+
+
+# # Que 7: What happens if an existing file is opened in write mode?
+
+# In[ ]:
+
+
+Answer 7:
+existing file is opened in write mode then it can be erased or overwritten. 
+
+
+# # Que 8: How do you tell the difference between read() and readlines()?
+
+# In[ ]:
+
+
+Answer 8: 
+read() method returns the file’s entire contents as a single string value. 
+The readlines() method returns a list of strings,
+where each string is a line from the file’s contents.
+
+
+# # Que 9:What data structure does a shelf value resemble?
+
+# In[ ]:
+
+
+Answer 9: 
+Shelf value resembles a dictionary value; it has keys and values. 
+
